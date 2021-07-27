2fea5a22cb64be8ae676593ec9798520a5b467b4
Samuel Alves Chagas <samuelchagasalves@gmail.com>
2021-07-26 23:00:02 -0300

    Criação de estrutura para o banco de dados sqlite

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import express from 'express';
+import routes from './routes'
 
 const app = express();
 
-app.get('/users', (req, res)=>{
-    res.json(['Diego', 'Marcos', 'Cleiton'])
-    console.log('listagem de usuários');
-})
+app.use(express.json())
+app.use(routes)
+
 
 app.listen(3333)
