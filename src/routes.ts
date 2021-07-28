257f6c03485c893235f830ba0a5b42cf8aa520db
Samuel Alves Chagas <samuelchagasalves@gmail.com>
2021-07-27 22:31:07 -0300

    Desenvolvimento de rota para criação do ponto de coleta

--- src/routes.ts
+++ src/routes.ts
@@ -1,19 +1,51 @@
-import express from 'express'
-import knex from './database/connection'
+import express from "express";
+import knex from "./database/connection";
 
-const routes = express.Router()
+const routes = express.Router();
 
-routes.get('/items', async (req, res)=>{
-    const items = await knex('items').select('*')
+routes.get("/items", async (req, res) => {
+  const items = await knex("items").select("*");
 
-    const serializedItems = items.map(item => {
-        return {
-            title: item.title,
-            image_url: `http://localhost:3333/uploads/${item.image}`,
-        }
-    })
+  const serializedItems = items.map((item) => {
+    return {
+      id: item.id,
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`,
+    };
+  });
 
-    return res.json(serializedItems)
-})
+  return res.json(serializedItems);
+});
 
-export default routes
+routes.post("/points", async (req, res) => {
+  const { name, email, whatsapp, latitude, longitude, city, uf, items } =
+    req.body;
+
+  const trx = await knex.transaction()
+  
+  const insertedIds = await trx("points").insert({
+    image: "image-fake",
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+  });
+
+  const point_id = insertedIds[0]
+
+  const pointItems = items.map((item_id: number) => {
+      return {
+          item_id,
+          point_id,
+      }
+  })
+
+  await trx('point_items').insert(pointItems)
+
+  return res.json({ success: true });
+});
+
+export default routes;
